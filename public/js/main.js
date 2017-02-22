a313a51d79f6087dc9bdd2c20736f68d8370631e
Francisco Javier Calvo <franciscojaviercalvoserrano@gmail.com>
2017-02-22 18:43:15 +0100

    Interfaz y funcionalidad(Falta capturar nick del usuario)

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,47 @@
 $(document).ready(function(){
     var socket = io();
+    var boton = $('.boton');
+    var boton_limpiar = $('.boton_limpieza');
+    var nombre_usuario ="Fran";
+    var listanombres = [];
+
+
     
-    var mensaje = $("#mensaje");
-    mensaje.keyup(function(e){
-        socket.emit('mensaje',mensaje.val());
+    socket.on('conectado',function(data){
+        $('.entorno_mensajes').append($(`<div class="mnsg_user">Se ha conectado ${nombre_usuario}</div>`)); 
+    });
+    
+    socket.on('mensaje_click',function(data){
+        if(data.length <= 0){
+            $('.entorno_mensajes').append($(`<div class="mnsg_user mnsg_error">${nombre_usuario}: *Mensaje vacio*</div>`)); 
+        }else{
+            $('.entorno_mensajes').append($(`<div class="mnsg_user">${nombre_usuario}: ${data}</div>`)); 
+        }
+    });
+    
+    boton.click(function(){
+        socket.emit('mensaje_click', $('#texto').val());
+        $('#texto').val('');
+    });
+    
+    /*limpieza de pantalla(solo la del usuario)*/
+    boton_limpiar.click(function(){
+        $('.entorno_mensajes').empty();
     });
     
     
+    socket.emit('nombre_usuario',nombre_usuario);
     
+    socket.on('nombre_usuario',function(data){
+        $('label.name_user').text(data);
+        $('#ListaUser').append($(`<div class="info_izq"><div class="muestra_user"><div id="imagen_lista" class="avatar" style="background-image: url(./img/avatar_1.png)"></div><p class="name">${data}</p></div></div>`));
+    });
     
+    $('#texto').keyup(function(event){
+       if (event.keyCode == 13){
+           socket.emit('mensaje_click', $('#texto').val());
+           $('#texto').val(''); 
+       } 
+    });
+
 });
--- server/server.js
+++ server/server.js
@@ -7,6 +7,25 @@ var io = require('socket.io')(http);
 app.use(express.static(path.join(__dirname,'../public')));
 
 
+io.on('connection',function(socket){
+    console.log("user connect");
+    socket.emit('conectado',"user conect");
+    
+    socket.on('mensaje_click',function(data){
+        io.emit('mensaje_click',data);
+    });
+    
+    
+    socket.on('enter',function(data){
+        console.log("enter");
+    });
+    
+    socket.on('nombre_usuario',function(data){
+        io.emit('nombre_usuario',data); 
+    });
+});
+
+
 /*Mensaje cuándo se produzca la conexión*/
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
